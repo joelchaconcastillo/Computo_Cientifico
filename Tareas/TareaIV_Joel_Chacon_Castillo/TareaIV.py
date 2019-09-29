fad1bb1b15b19b0b02c7cd3f006c8b86a52c12d9
Your Name <you@example.com>
2019-09-29 17:34:54 -0500

    ..

--- Tareas/TareaIV_Joel_Chacon_Castillo/TareaIV.py
+++ Tareas/TareaIV_Joel_Chacon_Castillo/TareaIV.py
@@ -12,7 +12,7 @@ np.set_printoptions(threshold=sys.maxsize)
 np.random.seed(3)
 def Kappa(A):
   BEigs = np.linalg.eigvals(A)
-  return np.max(BEigs)/np.min(BEigs)
+  return np.max(np.abs(BEigs))/np.min(np.abs(BEigs))
 
 #QR Factorization, Algorithm 23.1 taken of the Trefethen pag.175
 def QR(A):
@@ -22,7 +22,8 @@ def QR(A):
   R = np.zeros([n,n], dtype=float)
   for i in range(0,n):
     R[i,i] = np.sqrt(np.dot(V[:,i], V[:,i]))
-    Q[:,i] = V[:,i]/R[i,i]
+#    print(R[i,i])
+    Q[:,i] = V[:,i]/(R[i,i])
     for j in range(i+1, n):
         R[i,j] = np.dot(np.transpose(Q[:,i]),V[:,j])
         V[:,j] -= R[i,j]*Q[:,i] 
@@ -35,19 +36,25 @@ def Practical_QR_iterative(A):
     for i in range(dim,0,-1):
      while np.abs(Ak[i-1, i-2]) > Tol:
         muI = Ak[i-1, i-1]*np.identity(i)
-        Q,R = QR(Ak[0:i,0:i] - muI)
+        #Q,R = QR(Ak[0:i,0:i] - muI)
+        Q, R = scipy.linalg.qr( Ak[0:i,0:i]-muI,  mode='economic')
+	print("====Q====")
+	print(Q)
+	print("====R====")
+	print(R)
         Ak[0:i, 0:i] = np.dot(R, Q) + muI
+        print(Ak)
         QAugmented = np.identity(dim)
         QAugmented[0:i, 0:i] = np.copy(Q)
         Qf = (Qf).dot(QAugmented)
-    return Ak, np.transpose(Qf)
+    return Ak, (Qf)
 
 def Practical_QR_recursive(A):
     dim = (A[:,0]).size
     Ak = np.copy(A)#np.dot(np.dot( np.transpose(Q), np.copy(A)), Q)
     if dim == 1:
       return Ak[0]
-    Tol = 1e-10
+    Tol = 1e-2
     while np.abs(Ak[dim-1, dim-2]) > Tol:
        muI = Ak[dim-1,dim-1]*np.identity(dim)
        Q,R = QR(Ak - muI)
@@ -55,19 +62,21 @@ def Practical_QR_recursive(A):
     Ak[0:(dim-1),0:(dim-1)], Qf[0:(dim-1),0:(dim-1)] = Practical_QR(Ak[0:(dim-1),0:(dim-1)])
     return Ak
 def Exercise2():
-    N=1
+  for N in range(1,6):
+#    N=1
     epsilon = 10**N
     A = np.array([[8.0, 1.0, 0.0], [1.0, 4.0, epsilon], [0.0, epsilon, 1.0]])
-    print(A)
     Sigma, Q = Practical_QR_iterative(A)
+    print("=========================")
+    print(N)
+    print(Kappa(A))
+    print(A)
     print(Sigma)
     print(Q)
-    print(np.dot(np.dot(np.transpose(Q),Sigma), Q))
+    #print(np.dot(np.dot(np.transpose(Q),Sigma), Q))
 def Exercise5():
-    N=1
-    epsilon = 10**N
-    A = np.array([[8.0, 1.0, 0.0], [1.0, 4.0, epsilon], [0.0, epsilon, 1.0]])
-#    An = A / A.max(axis=0)
+    A = 1.0/3.0*np.array([[2.0, -2.0, 1.0], [1.0, 2.0, 2.0], [2.0, 1.0, -2.0]])
+    print(A) 
     Sigma, Q = Practical_QR_iterative(A)
     print(Sigma)
     print(Q)
