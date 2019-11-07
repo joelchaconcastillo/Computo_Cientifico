5530385bbf94cfcfeeca1336edd30055ccec0b8a
Your Name <you@example.com>
2019-11-06 18:05:13 -0600

    ..

--- Tareas/TareaVIII_Joel_Chacon_Castillo/TareaVIII.py
+++ Tareas/TareaVIII_Joel_Chacon_Castillo/TareaVIII.py
@@ -170,8 +170,103 @@ def Punto2():
   plt.hist(xd, label='CDF',histtype='step', alpha=0.8, density=True )
   print(xd)
   plt.show()
+def factorial(n):
+    return reduce((lambda x,y: x*y),range(1,n+1))
 #########################################Section of point 3
+def posteriori(pi, ti, alpha, gamma_p, delta, lambdas_i, beta, fpi, n):
+ # prod_lambda_t = np.multiply(lambdas_i, ti)
+ # L1 = np.exp(-prod_lambda_t - lambdas_i*beta - delta*beta) 
+ # L2 = np.power(prod_lambda_t, pi)/fpi
+ # L3 = (beta**alpha)*(np.power(lambdas_i, alpha-1.0))*(delta**gamma_p)*(beta**(gamma_p-1.0))
+ # return np.prod( np.multiply(L1, L2, L3))
+  #prod_lambda_t = np.multiply(lambdas_i, ti)
+  #L1 = np.exp(-prod_lambda_t)*np.power(prod_lambda_t, pi)/fpi
+  #L2 = gamma.pdf(alpha, beta)
+  #L3 = gamma.pdf(gamma_p, delta)
+  #return np.prod(L1)*L2*L3
+  L1 = np.sum(np.multiply(-ti, lambdas_i)) -beta*np.sum(lambdas_i-delta*beta)
+  L2 = beta**(n*alpha - gamma_p - 1.0)
+  L3 = np.prod( np.multiply(np.power(ti, pi),np.power(lambdas_i,pi+alpha-1))   )
+  return L1*L2*L3
+
+def q1_pump(t, p, i, alpha, x):
+  y = np.copy(x)
+  #y[i] = gamma.rvs(t[i-1]*p[i-1] + alpha, scale = x[0]+1)
+  y[i] = gamma.rvs(p[i-1] + alpha, scale = t[i-1]+x[0])
+  return y
+def density_q1_pump(ti, pi, alpha, lambdas_i, beta):
+  #return gamma.pdf(lambdas_i, ti*pi + alpha, scale = beta+1)
+  return gamma.pdf(lambdas_i, pi + alpha, scale =ti+beta)
+
+def q2_pump(x, i, n, alpha, gamma_p, delta):
+  y = np.copy(x)
+  sum_lambda = np.sum(x[1:])
+  y[i] = gamma.rvs(n*alpha+gamma_p, scale = delta + sum_lambda)
+  return y
+def density_q2_pump(x,y, n, alpha, gamma_p, delta):
+  sum_lambda = np.sum(y[1:])
+  #print(str(n*alpha+gamma_p) + " "+ str(delta + sum_lambda))
+  return gamma.pdf(x[0], n*alpha+gamma_p, scale = delta + sum_lambda)
+
+
+def Hybrid_Gibbs_Pump_Failures(n, ti, pi, alpha, gamma_p, delta, maxite, burnin):
+  xt = uniform.rvs(size=11)*0.002
+  xt[0] = 1
+  X = np.array([xt])
+  fpi = np.copy(pi)
+  for i in range(n):
+   fpi[i] = math.factorial(pi[i]) 
+  yt = xt
+  dyt = 0
+  dxt = 0
+  cont = 0
+  d=-1
+  while cont < maxite:
+   d=(d+1)%11
+#   d=0
+   ##d = np.random.randint(11) ##same probability of each kernel...
+   if d >=1:
+      yt = q1_pump(ti, pi, d, alpha, xt)
+      dyt = density_q1_pump(ti[d-1], pi[d-1], alpha, yt[d], yt[0])
+      dxt = density_q1_pump(ti[d-1], pi[d-1], alpha, xt[d], xt[0])
+   elif d == 0:
+      yt = q2_pump(xt, d, n, alpha, gamma_p, delta)
+      dyt = density_q2_pump(yt, xt, n, alpha, gamma_p, delta)
+      dxt = density_q2_pump(xt, yt, n, alpha, gamma_p, delta)
+   ##check support ##it is not in the support
+   if yt.all() > 3:
+      continue
+   fx = posteriori(pi, ti, alpha, gamma_p, delta, xt[1:], xt[0], fpi, n)
+   fy = posteriori(pi, ti, alpha, gamma_p, delta, yt[1:], yt[0], fpi, n)
+   rho =(fy/fx)*(dxt/dyt)
+   print(yt)
+   print(fx)
+   print(fy)
+   print(rho)
+   u = (uniform.rvs())
+   ##compute decision criteria
+   if u < rho: ##accepting..
+     if burnin > cont:
+        X = np.vstack((X, yt))
+     if (cont%100)==0:
+       print(str(cont) + " "+str(xt))
+     xt = np.copy(yt)
+     cont +=1
+  return X 
+
+
 def Punto3():
+ n = 10
+ ti = np.array([94.32, 15.72, 62.88, 125.76, 5.24, 31.44, 1.05, 1.05, 2.1, 10.48])
+ pi = np.array([5.0, 1.0, 5.0, 14.0, 3.0, 18.0, 1.0, 1.0, 4.0, 22.0])
+ alpha = 1.8
+ gamma_p = 0.01
+ delta = 1.0
+ burnin = 100
+ maxite = 100000
+ x = Hybrid_Gibbs_Pump_Failures(n, ti, pi, alpha, gamma_p, delta, maxite, burnin)
+ print(x)
+ print("k")
 
 #Punto1()
 #Punto2()
