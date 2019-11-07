f16f14d3940b434875d401eec01ab6e72152d508
Joel <joel.chacon@cimat.mx>
2019-11-07 00:52:13 -0600

    ..

--- Tareas/TareaVIII_Joel_Chacon_Castillo/TareaVIII.py
+++ Tareas/TareaVIII_Joel_Chacon_Castillo/TareaVIII.py
@@ -17,7 +17,7 @@ np.random.seed(3)
 def qi(X, Mu, v_sigma, rho, i): #X_i | X_j
       j = 1-i
       Mu_i_j = Mu[i] + rho*(v_sigma[i]/v_sigma[j])*(X[j]-Mu[j])
-      Sigma_i_j = (v_sigma[i]**2)*(1.0-rho**2)
+      Sigma_i_j = (v_sigma[i]**2)*(1.0-(rho**2))
       yt = np.copy(X)
       yt[i] = norm.rvs(Mu_i_j, Sigma_i_j)
       return yt
@@ -34,40 +34,56 @@ def energy(xt, yt, Mu, Sigma_Inv, v_sigma, rho, i):   #it is computed trough the
 
 def MH(maxite, Mu, Sigma, rho, v_sigma):
     Sigma_Inv = linalg.inv(Sigma) #precision matrix..
-    U = (uniform.rvs(2)*2-1.0)*0.2
+    U = (uniform.rvs(size=2)*2-3.0)
     xt = Mu+U# np.array([Mu[0]+uniform.rvs()*0.1, Mu[1]+uniform.rvs()*0.1])
     X = np.array([xt])
     cont = 0
     while cont < maxite:
-       for k in range(2):
-          d = np.random.randint(2)
-          yt = qi(xt, Mu, v_sigma, rho, d)
-	  ##check support ##it is not in the support
-          if yt[d]==0: 
-            continue	  
-          criteria = energy(xt, yt, Mu, Sigma_Inv, v_sigma, rho, d)
-          u = np.log(uniform.rvs())
-	  ##compute decision criteria
-          if u < criteria: ##accepting..
-            X = np.vstack((X, yt))
-            xt = np.copy(yt)
-            cont +=1
+      d = np.random.randint(2)
+      yt = qi(xt, Mu, v_sigma, rho, d)
+      ##check support ##it is not in the support
+      if yt[d]==0: 
+        continue	  
+####In this case the accepting criteria is not required..
+#      criteria = energy(xt, yt, Mu, Sigma_Inv, v_sigma, rho, d)
+#      u = np.log(uniform.rvs())
+#      if u < criteria: ##accepting..
+      ##compute decision criteria
+      X = np.vstack((X, yt))
+      xt = np.copy(yt)
+      cont +=1
     return X 
 def Punto1():
-   rho = 0.99
+   rho = 0.85
+   maxite = 10000
    v_sigma = np.array([1,1]) # sigma vector
    Mu = np.array([1, 1])
    Sigma = np.array([[v_sigma[0]**2, rho*v_sigma[0]*v_sigma[1]], [rho*v_sigma[0]*v_sigma[1], v_sigma[1]**2]])
-   x = MH(10000, Mu, Sigma, rho, v_sigma)
+   x = MH(maxite, Mu, Sigma, rho, v_sigma)
    ##print contour ...
-   xl, yl = np.mgrid[-1:3:.01, -1:3:.01]
+   xl, yl = np.mgrid[-3:5:.01, -3:5:.01]
    pos = np.empty(xl.shape + (2,))
    pos[:, :, 0] = xl
    pos[:, :, 1] = yl
    rv = multivariate_normal(Mu, Sigma)
    plt.contourf(xl, yl, rv.pdf(pos))
-   plt.plot( x[:,0], x[:,1], '.r')
+   plt.plot( x[:,0], x[:,1], '-r')
+   plt.title("Simulacion de districion normal multivariada con \n " + r"$\rho$=" + str(rho) + r" $\mu$="+str(Mu)+" y max. ite.="+str(maxite) )
    plt.show()
+   ####
+   plt.plot(np.abs(x[1:maxite, 0]-x[0:(maxite-1), 0]))
+   plt.xlabel("Tiempo")
+   plt.ylabel("Tamanio de paso")
+   plt.title("Desplazamientos en la variable " r"$X_1$ con " r"$\rho = $"+str(rho) )
+   plt.show()
+   plt.plot(np.abs(x[1:maxite, 1]-x[0:(maxite-1), 1]))
+   plt.xlabel("Tiempo")
+   plt.ylabel("Tamanio de paso")
+   plt.title("Desplazamientos en la variable " r"$X_2$ con " r"$\rho = $"+str(rho) )
+   plt.show()
+
+
+
 ########################################################Section point 2
 ###X[0] --> alpha
 ###X[1] --> lambda
@@ -191,83 +207,55 @@ def posteriori(pi, ti, alpha, gamma_p, delta, lambdas_i, beta, fpi, n):
 
 def q1_pump(t, p, i, alpha, x):
   y = np.copy(x)
-  #y[i] = gamma.rvs(t[i-1]*p[i-1] + alpha, scale = x[0]+1)
-  y[i] = gamma.rvs(p[i-1] + alpha, scale = t[i-1]+x[0])
+  y[i] = gamma.rvs(p[i-1] + alpha, scale = 1.0/(t[i-1]+x[0]))
   return y
-def density_q1_pump(ti, pi, alpha, lambdas_i, beta):
-  #return gamma.pdf(lambdas_i, ti*pi + alpha, scale = beta+1)
-  return gamma.pdf(lambdas_i, pi + alpha, scale =ti+beta)
-
 def q2_pump(x, i, n, alpha, gamma_p, delta):
   y = np.copy(x)
   sum_lambda = np.sum(x[1:])
-  y[i] = gamma.rvs(n*alpha+gamma_p, scale = delta + sum_lambda)
+  y[i] = gamma.rvs(n*alpha+gamma_p, scale = 1.0/(delta + sum_lambda))
   return y
-def density_q2_pump(x,y, n, alpha, gamma_p, delta):
-  sum_lambda = np.sum(y[1:])
-  #print(str(n*alpha+gamma_p) + " "+ str(delta + sum_lambda))
-  return gamma.pdf(x[0], n*alpha+gamma_p, scale = delta + sum_lambda)
-
 
 def Hybrid_Gibbs_Pump_Failures(n, ti, pi, alpha, gamma_p, delta, maxite, burnin):
-  xt = uniform.rvs(size=11)*0.002
-  xt[0] = 1
+  xt = uniform.rvs(size=11)*0.02
   X = np.array([xt])
   fpi = np.copy(pi)
   for i in range(n):
    fpi[i] = math.factorial(pi[i]) 
   yt = xt
-  dyt = 0
-  dxt = 0
   cont = 0
   d=-1
   while cont < maxite:
    d=(d+1)%11
-#   d=0
-   ##d = np.random.randint(11) ##same probability of each kernel...
+#   d = np.random.randint(11) ##same probability of each kernel...
    if d >=1:
       yt = q1_pump(ti, pi, d, alpha, xt)
-      dyt = density_q1_pump(ti[d-1], pi[d-1], alpha, yt[d], yt[0])
-      dxt = density_q1_pump(ti[d-1], pi[d-1], alpha, xt[d], xt[0])
    elif d == 0:
       yt = q2_pump(xt, d, n, alpha, gamma_p, delta)
-      dyt = density_q2_pump(yt, xt, n, alpha, gamma_p, delta)
-      dxt = density_q2_pump(xt, yt, n, alpha, gamma_p, delta)
    ##check support ##it is not in the support
-   if yt.all() > 3:
-      continue
-   fx = posteriori(pi, ti, alpha, gamma_p, delta, xt[1:], xt[0], fpi, n)
-   fy = posteriori(pi, ti, alpha, gamma_p, delta, yt[1:], yt[0], fpi, n)
-   rho =(fy/fx)*(dxt/dyt)
-   print(yt)
-   print(fx)
-   print(fy)
-   print(rho)
-   u = (uniform.rvs())
+#   fx = posteriori(pi, ti, alpha, gamma_p, delta, xt[1:], xt[0], fpi, n)
    ##compute decision criteria
-   if u < rho: ##accepting..
-     if burnin > cont:
-        X = np.vstack((X, yt))
-     if (cont%100)==0:
-       print(str(cont) + " "+str(xt))
+   if burnin > cont:
+     X = np.vstack((X, yt))
+   if (cont%1000)==0:
+     print(str(cont) + " "+str(xt))
      xt = np.copy(yt)
-     cont +=1
+   cont +=1
   return X 
 
 
 def Punto3():
  n = 10
  ti = np.array([94.32, 15.72, 62.88, 125.76, 5.24, 31.44, 1.05, 1.05, 2.1, 10.48])
- pi = np.array([5.0, 1.0, 5.0, 14.0, 3.0, 18.0, 1.0, 1.0, 4.0, 22.0])
+ pi = np.array([5.0, 1.0, 5.0, 14.0, 3.0, 19.0, 1.0, 1.0, 4.0, 22.0])
  alpha = 1.8
  gamma_p = 0.01
  delta = 1.0
  burnin = 100
- maxite = 100000
+ maxite = 1000000
  x = Hybrid_Gibbs_Pump_Failures(n, ti, pi, alpha, gamma_p, delta, maxite, burnin)
  print(x)
  print("k")
 
-#Punto1()
+Punto1()
 #Punto2()
-Punto3()
+#Punto3()
